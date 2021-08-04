db9fc4bd6df9e6773f6d75845e226f40e429dc27
chanyouvita <chanyouvita.kosign@gmail.com>
2021-08-04 12:54:38 +0700

    [Added] - Shared preference security crypto.

---
+++ applibrary/src/main/java/com/bnkc/library/di/AppModule.kt
@@ -0,0 +1,25 @@
+/**
+ * KOSIGN (Cambodia) Investment Co., Ltd.
+ * @author chan youvita
+ * @since 2021.08.04
+ */
+package com.bnkc.library.di
+
+import android.content.Context
+import com.bnkc.library.prefer.CredentialSharedPrefer
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideCredentialSharedPreference(@ApplicationContext context: Context) = CredentialSharedPrefer.getInstance(context)
+
+}
