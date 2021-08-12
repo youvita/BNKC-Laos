2bfd037cdbcf7a8594efa4902de0ca784330219e
Nazimah Sren <7329007-nazimahsren@users.noreply.gitlab.com>
2021-08-12 08:21:17 +0700

    update layout and add string

--- customer/src/main/java/com/mobile/bnkcl/ui/signup/SignUpViewModel.kt
+++ customer/src/main/java/com/mobile/bnkcl/ui/signup/SignUpViewModel.kt
@@ -1,8 +1,22 @@
 package com.mobile.bnkcl.ui.signup
 
 import com.bnkc.sourcemodule.base.BaseViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
+import com.mobile.bnkcl.data.repository.signup.SignUpRepo
+import com.mobile.bnkcl.data.request.signup.PreSignUpRequest
+import com.mobile.bnkcl.data.request.signup.SignUpRequest
+import com.mobile.bnkcl.data.response.signup.PreSignUpResponse
+import com.mobile.bnkcl.data.response.signup.SignUpResponse
+import javax.inject.Inject
 
-class SignUpViewModel : BaseViewModel {
-    constructor() : super()
+class SignUpViewModel @Inject constructor(private val signUpRepo: SignUpRepo) : BaseViewModel() {
+
+   lateinit var signUpRequest : SignUpRequest
+   lateinit var preSignUpRequest: PreSignUpRequest
+   lateinit var signUpResponse: SignUpResponse
+   lateinit var preSignUpResponse: PreSignUpResponse
+
+
+
+
+
 }
---
+++ customer/src/main/java/com/mobile/bnkcl/ui/success/AskBNKCRegisterFailedActivity.kt
@@ -0,0 +1,12 @@
+package com.mobile.bnkcl.ui.success
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mobile.bnkcl.R
+
+class AskBNKCRegisterFailedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ask_b_n_k_c_register_failed)
+    }
+}
