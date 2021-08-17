ae37cf16936dd719e3af94536204a1087f973224
chanyouvita <chanyouvita.kosign@gmail.com>
2021-08-17 10:55:46 +0700

    [Modified] - Language changed

--- customer/src/main/java/com/mobile/bnkcl/ui/dialog/LanguageDialog.kt
+++ customer/src/main/java/com/mobile/bnkcl/ui/dialog/LanguageDialog.kt
@@ -1,62 +1,71 @@
 package com.mobile.bnkcl.ui.dialog
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
+import android.widget.RadioGroup
+import com.bnkc.library.app.recreateLanguageChanged
 import com.bnkc.library.prefer.CredentialSharedPrefer
 import com.bnkc.library.util.LocaleHelper
 import com.bnkc.sourcemodule.base.BaseDialogFragment
 import com.mobile.bnkcl.R
 import com.mobile.bnkcl.databinding.DialogLanguageBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class LanguageDialog : BaseDialogFragment<DialogLanguageBinding>(), View.OnClickListener {
+class LanguageDialog : BaseDialogFragment<DialogLanguageBinding>(), RadioGroup.OnCheckedChangeListener {
 
     @Inject
     lateinit var sharedPrefer: CredentialSharedPrefer
 
     private var langSelectedListener: (() -> Unit)? = null
 
+    private lateinit var languageCode: String
+
     override fun getLayoutId(): Int {
         return R.layout.dialog_language
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.rbLaos.setOnClickListener(this)
-        binding.rbEn.setOnClickListener(this)
-
-        val preLang : String = sharedPrefer.getPrefer("pre_lang").toString()
-        setSelectedLang(preLang)
+    companion object {
+        @SuppressLint("ConstantLocale")
+        private val getLanguages: MutableList<String> = mutableListOf(
+                "lo",
+                Locale.ENGLISH.language
+        )
     }
 
-    private fun setSelectedLang(pre_lang : String) {
-        if (pre_lang == "en") {
-            binding.rbEn.isChecked = true
-        } else {
-            binding.rbLaos.isChecked = true
-        }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        languageCode = LocaleHelper.getLanguage(requireContext())
+
+        setCheckRadioButton()
     }
 
     fun onLangSelected(langSelectedListener: (() -> Unit)) =
         apply { this.langSelectedListener = langSelectedListener }
 
-    override fun onClick(v: View?) {
-        when (v!!.id) {
-            R.id.rb_laos -> {
-                LocaleHelper.setLocale(requireContext(), "lo")
-                sharedPrefer.putPrefer("pre_lang", "lo")
-                langSelectedListener?.invoke()
-                dialog?.dismiss()
-            }
-            R.id.rb_en -> {
-                LocaleHelper.setLocale(requireContext(), "en")
-                sharedPrefer.putPrefer("pre_lang", "en")
-                langSelectedListener?.invoke()
-                dialog?.dismiss()
-            }
+    private fun setCheckRadioButton() {
+        when (languageCode) {
+            Locale.ENGLISH.language -> binding.rbEn.isChecked = true
+            else -> binding.rbLaos.isChecked = true
+        }
+        binding.rgLanguage.setOnCheckedChangeListener(this)
+    }
+
+    private fun changeLanguage(language: String) {
+        Handler().postDelayed({
+            LocaleHelper.setLanguage(requireContext(), language)
+            recreateLanguageChanged()
+        }, 100L)
+    }
+
+    override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
+        when(checkedId) {
+            R.id.rb_laos -> {changeLanguage(getLanguages[0])}
+            R.id.rb_en -> {changeLanguage(getLanguages[1])}
         }
     }
 }
--- customer/src/main/java/com/mobile/bnkcl/ui/home/HomeActivity.kt
+++ customer/src/main/java/com/mobile/bnkcl/ui/home/HomeActivity.kt
@@ -2,13 +2,13 @@ package com.mobile.bnkcl.ui.home
 
 import android.content.Intent
 import android.os.Bundle
-import com.bnkc.library.util.LocaleHelper
 import com.bnkc.sourcemodule.base.BaseActivity
 import com.mobile.bnkcl.R
 import com.mobile.bnkcl.databinding.ActivityHomeBinding
 import com.mobile.bnkcl.ui.dialog.LanguageDialog
 import com.mobile.bnkcl.ui.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 
 @AndroidEntryPoint
 class HomeActivity : BaseActivity<ActivityHomeBinding>() {
@@ -22,6 +22,8 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>() {
 
         initLanguage()
 
+        binding.localeCode = Locale.getDefault().language
+
         binding.clLease.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
@@ -31,23 +33,6 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>() {
         binding.clChangeLanguage.setOnClickListener {
             val languageDialog = LanguageDialog()
             languageDialog.show(supportFragmentManager, languageDialog.tag)
-
-            languageDialog.onLangSelected {
-                val context = LocaleHelper.setLocale(this, LocaleHelper.getLanguage(this))
-                binding.tvCurrentLang.text = context.getString(R.string.home_04)
-                binding.tvLoan.text = context.getString(R.string.home_01)
-                binding.tvLease.text = context.getString(R.string.home_02)
-                binding.tvLeaseEx.text = context.getString(R.string.home_03)
-
-                if (LocaleHelper.getLanguage(this) == "en"){
-                    binding.ivFlag.setImageResource(R.drawable.ic_avatar_l)
-                } else {
-                    binding.ivFlag.setImageResource(R.drawable.laos_flag)
-                }
-
-                sharedPrefer.putPrefer("lang", LocaleHelper.getLanguage(this))
-            }
         }
-
     }
 }
