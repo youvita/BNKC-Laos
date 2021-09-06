d8a3b5779de62a6fb446dc93ffc02abad3e0122c
Nazimah Sren <7329007-nazimahsren@users.noreply.gitlab.com>
2021-09-06 17:43:09 +0700

    add date of birth

--- customer/src/main/java/com/mobile/bnkcl/ui/signup/SignUpActivity.kt
+++ customer/src/main/java/com/mobile/bnkcl/ui/signup/SignUpActivity.kt
@@ -1,8 +1,12 @@
 package com.mobile.bnkcl.ui.signup
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import com.bnkc.sourcemodule.base.BaseActivity
 import com.bnkc.sourcemodule.dialog.DatePickerDialog
 import com.bnkc.sourcemodule.dialog.ListChoiceDialog
@@ -12,7 +16,11 @@ import com.mobile.bnkcl.data.response.area.AreaItems
 import com.mobile.bnkcl.databinding.ActivitySignUpBinding
 import dagger.hilt.android.AndroidEntryPoint
 import io.reactivex.disposables.Disposable
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
+import kotlin.collections.ArrayList
 
 @AndroidEntryPoint
 class SignUpActivity : BaseActivity<ActivitySignUpBinding>() , View.OnClickListener{
@@ -26,6 +34,8 @@ class SignUpActivity : BaseActivity<ActivitySignUpBinding>() , View.OnClickListe
     private var signUpDisposable: Disposable? = null
     private var selectedItem = 0
 
+    private var myCalendar = Calendar.getInstance()
+
     override fun getLayoutId(): Int = R.layout.activity_sign_up
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,14 +44,17 @@ class SignUpActivity : BaseActivity<ActivitySignUpBinding>() , View.OnClickListe
         observeCapitalArea()
         observeAddressData()
 
+        binding.ivBack.setOnClickListener(this)
         /**
          *
          * */
+        binding.tvDob.addTextChangedListener(mDateWatcher)
+
         binding.llDob.setOnClickListener {
             datePickerDialog.show(supportFragmentManager, datePickerDialog.tag)
             datePickerDialog.onDateSelected {
-                binding.tvDob.text
-
+                Log.d("Date: ", "onCreate: $it")
+                binding.tvDob.setText(it)
             }
         }
 
@@ -50,18 +63,20 @@ class SignUpActivity : BaseActivity<ActivitySignUpBinding>() , View.OnClickListe
         binding.lltAddress.llCapital.setOnClickListener {
             if (objects != null && objects!!.size > 0){
                 listChoiceDialog = ListChoiceDialog.newInstance(
-                        R.drawable.ic_badge_general,
-                        getString(R.string.addition_capital),
-                        addressInfoViewModel.setUpAreaName(objects!!),
-                        selectedItem
+                    R.drawable.ic_badge_general,
+                    getString(R.string.addition_capital),
+                    addressInfoViewModel.setUpAreaName(objects!!),
+                    selectedItem
                 )
 
-                listChoiceDialog.setOnItemListener ={
-                    a : Int ->
+                listChoiceDialog.setOnItemListener ={ a: Int ->
                     selectedItem = a
                     binding.lltAddress.tvCapital.text = objects!![a].alias1
 
-                    addressInfoViewModel.addressData = AddressData("DISTRICT",objects!![a].id.toString())
+                    addressInfoViewModel.addressData = AddressData(
+                        "DISTRICT",
+                        objects!![a].id.toString()
+                    )
                     addressInfoViewModel.getAddress()
 
                 }
@@ -74,18 +89,20 @@ class SignUpActivity : BaseActivity<ActivitySignUpBinding>() , View.OnClickListe
         binding.lltAddress.llDistrict.setOnClickListener{
             if (objects != null && objects!!.size>0){
                 listChoiceDialog = ListChoiceDialog.newInstance(
-                        R.drawable.ic_badge_general,
-                        "District",
-                        addressInfoViewModel.setUpAreaName(objects!!),
-                        selectedItem
+                    R.drawable.ic_badge_general,
+                    "District",
+                    addressInfoViewModel.setUpAreaName(objects!!),
+                    selectedItem
                 )
 
-                listChoiceDialog.setOnItemListener = {
-                    b : Int ->
+                listChoiceDialog.setOnItemListener = { b: Int ->
                     selectedItem = b
                     binding.lltAddress.tvDistrict.text = objects!![b].alias1
 
-                    addressInfoViewModel.addressData = AddressData("VILLAGE",objects!![b].id.toString())
+                    addressInfoViewModel.addressData = AddressData(
+                        "VILLAGE",
+                        objects!![b].id.toString()
+                    )
                     addressInfoViewModel.getAddress()
                 }
 
@@ -97,14 +114,13 @@ class SignUpActivity : BaseActivity<ActivitySignUpBinding>() , View.OnClickListe
         binding.lltAddress.llVillage.setOnClickListener{
             if (objects != null && objects!!.size>0){
                 listChoiceDialog = ListChoiceDialog.newInstance(
-                        R.drawable.ic_badge_general,
-                        "Village",
-                        addressInfoViewModel.setUpAreaName(objects!!),
-                        selectedItem
+                    R.drawable.ic_badge_general,
+                    "Village",
+                    addressInfoViewModel.setUpAreaName(objects!!),
+                    selectedItem
                 )
 
-                listChoiceDialog.setOnItemListener = {
-                    b : Int ->
+                listChoiceDialog.setOnItemListener = { b: Int ->
                     selectedItem = b
                     binding.lltAddress.tvVillage.text = objects!![b].alias1
                 }
@@ -130,6 +146,66 @@ class SignUpActivity : BaseActivity<ActivitySignUpBinding>() , View.OnClickListe
        }
     }
 
+
+
+     private val mDateWatcher: TextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+        override fun afterTextChanged(s: Editable) {
+            val text = binding.tvDob.text.toString()
+            val textLength: Int = binding.tvDob.text!!.length
+            if (text.endsWith("-") || text.endsWith(" ")) return
+            if (textLength == 3) {
+                binding.tvDob.setText(
+                    StringBuilder(text).insert(text.length - 1, "-").toString()
+                )
+                binding.tvDob.setSelection(
+                    binding.tvDob.text.toString().length
+                )
+            } else if (textLength == 6) {
+                binding.tvDob.setText(
+                    StringBuilder(text).insert(text.length - 1, "-").toString()
+                )
+                binding.tvDob.setSelection(
+                    binding.tvDob.text.toString().length
+                )
+            }
+            if (textLength == 10) {
+                setStringDateToCalendar(s.toString())
+            }
+            validateUserInfo()
+        }
+    }
+    private fun setStringDateToCalendar(stDate: String) {
+        try {
+            if (stDate.length == 10) {
+                val sdf = SimpleDateFormat("dd-MM-yyyy", Locale.US)
+                val date = sdf.parse(stDate)
+                myCalendar.time = date
+            }
+        } catch (e: ParseException) {
+            e.printStackTrace()
+        }
+    }
+    private fun validateUserInfo() {
+        if (binding.tvDob.text.toString() != ""
+            && binding.tvDob.text.toString().length === 10
+            && binding.edtName.text.toString() != ""
+            && binding.tvGender.text.toString() !=""
+        ) {
+            binding.btnNext.setOnClickListener(this)
+            binding.btnNext.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.selector_d7191f_8b0304
+            )
+        } else {
+            binding.btnNext.isClickable = false
+            binding.btnNext.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.round_solid_e1e5ec_8
+            )
+        }
+    }
 
 
     override fun onClick(v: View?) {
