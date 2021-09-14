17dbeefc78666ccc799ea2c30f41c1aeea3b45eb
chanyouvita <chanyouvita.kosign@gmail.com>
2021-09-14 17:32:09 +0700

    [Modified] - clean code

--- sourcemodule/src/main/java/com/bnkc/sourcemodule/binding/BindingAdapters.kt
+++ sourcemodule/src/main/java/com/bnkc/sourcemodule/binding/BindingAdapters.kt
@@ -7,8 +7,10 @@ package com.bnkc.sourcemodule.binding
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
@@ -144,6 +146,44 @@ object BindingAdapters {
     fun setCurrencyFormat(textView: TextView, str: String?) {
         if (str == null) return
         textView.text = FormatUtils.getNumberFormat(textView.context, str.split(" ")[1])
+    }
+
+    @JvmStatic
+    @BindingAdapter("active", "backgroundTextColor")
+    fun setActiveOrNormal(view: View, active : Boolean?, bgTextColor : Int?) {
+        if (view is TextView){
+            if (active!!) {
+                view.setTextColor(ContextCompat.getColor(view.context, R.color.color_ffffff))
+            } else {
+                if (bgTextColor != 0) view.setTextColor(bgTextColor!!)
+                else view.setTextColor(ContextCompat.getColor(view.context, R.color.color_90a4ae))
+            }
+        }else if (view is LinearLayout){
+            if (active!!) {
+                view.background = ContextCompat.getDrawable(view.context, R.drawable.selector_d7191f_8b0304)
+            } else {
+                if (bgTextColor != 0)view.background = ContextCompat.getDrawable(view.context, bgTextColor!!)
+                else view.background = ContextCompat.getDrawable(view.context, R.drawable.round_solid_e1e5ec_8)
+            }
+        }
+
+    }
+
+    @JvmStatic
+    @BindingAdapter("isSuccess")
+    fun ValidateButton.setSuccessOrFailButton(success : Boolean?) {
+        status = success!!
+        if (!success){
+            background = R.drawable.selector_d7191f_ffffee
+            textColor = ContextCompat.getColor(context, R.color.colorPrimary)
+        }
+
+    }
+
+    @JvmStatic
+    @BindingAdapter("label")
+    fun setLabelButton(view : ValidateButton,label : String) {
+        view.setValue(label)
     }
 
 }
